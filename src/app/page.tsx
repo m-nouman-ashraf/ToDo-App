04e1de92a98d6cd2e8975bdae569d1e5593fbdb6
Nouman Ashraf <muhammad-nouman@hotmail.com>
2023-10-16 18:44:07 +0500

    comment code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,17 +47,16 @@ export default async function Home() {
     </main>
   );
 }
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
+function CrudShowcase() {
+  // const latestPost = await api.post.getLatest.query();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
-      )}
+      )} */}
 
       <CreatePost />
     </div>
